c867204a39c9ee20e0d7790f9912631e0db7dd27
ShahdAbuodeh <77918654+ShahdAbuodeh@users.noreply.github.com>
2021-01-28 14:59:17 +0200

    Revert "test revert" hi from revert This reverts commit 9d1bd36a5d71bb358919fd2032b3fe58cfb0f4f4.

--- src/Start.java
+++ src/Start.java
@@ -5,7 +5,7 @@ class Start
         CalculatorForm g = new CalculatorForm();
         g.setVisible(true);
         System.out.println("hello ");
-        System.out.println("hello2 ");
+        System.out.println("hello1gitt ");
 
     }
 }
